ae045411ea3059e0a91e1ec8462d1f139d75dc2c
Anara Akmatalieva <anara.bakyt@gmail.com>
2022-02-01 20:53:08 +0600

    cards with react

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
 import React from "react";
 import reactLogo from "./img/react.png";
+import Card from "./components/Card";
 
 class App extends React.Component {
   render() {
     return (
       <>
+     
         <div className="contacts">
-          <div className="contact-card">
-            <img src={reactLogo} alt="this is a react logo" />
-            <h3>This is React</h3>
-            <div class="info-group">
-              <p>My phone number here</p>
-            </div>
-            <div className="info-group">
-              <p> this is my address </p>
-            </div>
-          </div>
-          <div className="contact-card">
-            <img src={reactLogo} alt="this is a react logo" />
-            <h3>This is React</h3>
-            <div class="info-group">
-              <p>My phone number here</p>
-            </div>
-            <div className="info-group">
-              <p> this is my address </p>
-            </div>
-          </div>
+        <Card image={reactLogo}/>
+        <Card image={reactLogo}/>
+          
         </div>
       </>
     );
